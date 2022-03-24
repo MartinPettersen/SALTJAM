af255168db1781c3539ebe1c613707c47dace0fd
Salt Dev <saltdev@Salts-MBP-2.localdomain>
2022-03-24 13:28:04 +0100

    change the name

--- starter-gatsby-blog/src/components/navigation.js
+++ starter-gatsby-blog/src/components/navigation.js
@@ -22,7 +22,7 @@ const Navigation = () => (
       </li>
       <li className={styles.navigationItem}>
         <Link to="/products/" activeClassName="active">
-          Products
+          Product
         </Link>
       </li>
     </ul>
