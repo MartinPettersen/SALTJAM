90bd0a4247c0993f268e101f8d3f6f9a8af55bd6
Salt Dev <saltdev@Salts-MBP-2.localdomain>
2022-03-24 11:46:02 +0100

    add git config

---
+++ starter-gatsby-blog/src/pages/products.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+
+import Seo from '../components/seo'
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import ProductPreview from '../components/product-preview'
+
+class ProductsIndex extends React.Component {
+  render() {
+    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
+
+    return (
+      <Layout location={this.props.location}>
+        <Seo title="Products" />
+        <Hero title="Products" />
+        <ProductPreview posts={posts}/>
+      </Layout>
+    )
+  }
+}
+
+export default ProductsIndex
+
+export const pageQuery = graphql`
+  query ProductsIndexQuery {
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+      nodes {
+        title
+        slug
+        publishDate(formatString: "MMMM Do, YYYY")
+        tags
+        heroImage {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            width: 424
+            height: 212
+          )
+        }
+        description {
+          childMarkdownRemark {
+            html
+          }
+        }
+      }
+    }
+  }
+`
