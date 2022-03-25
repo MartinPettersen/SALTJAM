a25788115c7d81d175aeedb914b1d25709d91717
Salt Dev <saltdev@Salts-MBP-2.localdomain>
2022-03-25 13:21:00 +0100

    display quantities

--- src/components/product-preview.js
+++ src/components/product-preview.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
@@ -7,32 +7,28 @@ import Tags from './tags'
 import * as styles from './article-preview.module.css'
 
 const ProductPreview = ({ products }) => {
+  const [quantities, setQuantities] = useState([]);
+  useEffect(() => {
+    fetch("/.netlify/functions/database")
+    .then(response => response.json())
+    .then(resultData => {
+      setQuantities(resultData)
+     })
+   }, [])
   if (!products) return null
   if (!Array.isArray(products)) return null
-
-  const fetchData = async (product) => {
-    const newGetRequest = await fetch("/.netlify/functions/database");
-    const newListJson = await newGetRequest.json();
-    const found = newListJson.find(element => element.slug === product.slug)
-    return found.quantity;
-  }
-
-  useEffect(() => {
-
-  }, [fetchData])
-  
-  
-  console.log(products)
   
   return (
     <Container>
       <ul className={styles.articleList}>
         {products.map((product) => {
+          const found = quantities.find(element => element.slug === product.slug)
           return (
             <li key={product.slug}>
                 <h2 className={styles.title}>{product.title}</h2>
                 <p>Price: <span>{product.price}</span>$</p>
-                <p>Quantity: <span>{}</span></p> 
+                {found? (<p>Quantity: <span>{found.quantity}</span></p>) : (<></>)}
+                
               <i><div
                 dangerouslySetInnerHTML={{
                   __html: product.description.childMarkdownRemark.html,
